fabce4bf92c392c0d883397d1cc90c14ee742dab
TRyan <122729309+TRyan09@users.noreply.github.com>
2023-04-13 16:31:15 +0200

    Update swol_file_compiler.py

--- swol_file_compiler.py
+++ swol_file_compiler.py
@@ -1,18 +1,27 @@
-# command: python swol_file_compiler.py [-c] filepath
+# command: "python swol_file_compiler.py [--compile] [--output OUTPUT] filepath"
 
 import argparse
 from SwolModule import SWOL
 from time import strftime, sleep
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--compile", action="store_true", help="Compile the script to a python file (will be stored in the compiled directory)")
+parser = argparse.ArgumentParser(prog="SWOL File Compiler", description="Compile a SWOL script to python", epilog="Language made by Tryan09")
+parser.add_argument("-c", "--compile", action="store_true", help="Compile the script to a python file")
 parser.add_argument("filepath", help="File path to your script")
+parser.add_argument("-o", "--output", help="File path to the output python file", required=False)
 args = parser.parse_args()
 
 print("SWOL by Tryan09\n")
 
 is_compiled = args.compile
 script_path = args.filepath
+
+if args.output == None:
+    current_date = strftime("%d-%m-%Y-%H-%M-%S")
+    output_path = f"compiled/{current_date}.py"
+elif args.output[-3:] == ".py":
+    output_path = args.output
+else:
+    output_path = args.output + ".py"
 
 swol = SWOL()
 
@@ -24,8 +33,7 @@ python_code = swol.compile_to_py(swol_script)
 print(python_code+"\n")
 
 if is_compiled:
-    current_date = strftime("%d-%m-%Y-%H-%M-%S")
-    with open(f"compiled/{current_date}.py", "x") as compiled_file:
+    with open(output_path, "x") as compiled_file:
         compiled_file.write(python_code)
 else:
     exec(python_code)
